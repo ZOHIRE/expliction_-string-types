ce6fcd0af77c453bf755e57da42b3287294a718f
ZOHIRE <gladyateur88@gmail.com>
2018-06-03 20:39:04 +0200

    Add project files.

---
+++ expliction_ string types/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace expliction__string_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //\' \" \\ \0  \a  \b  \n  \r  \t  \v  \f
+           
+
+            String mystring = "my djob is gazie bat \'alov informatic\' tinkyou ";
+            Console.WriteLine(" {0} ",mystring);
+            Console.ReadKey();
+        }
+    }
+}
